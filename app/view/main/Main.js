0be1b10c94bfa6dc0f612e07821ef35b7d25c7bf
15075767858 <a5461789>
2016-05-12 22:38:37 +0800

    弄好了加载modob

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -69,6 +69,8 @@ Ext.define('svgxml.view.main.Main', {
 });
 
 
+var WeekArr = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
 //week
 Ext.create('Ext.window.Window', {
         id: "drawWindow",
@@ -84,7 +86,8 @@ Ext.create('Ext.window.Window', {
         buttons: [
             {
                 text: "Ok", handler: function () {
-                var WeekArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
                 var weekly = {
                     "Weekly_Schedule": {}
                 }
@@ -94,16 +97,28 @@ Ext.create('Ext.window.Window', {
                     if (dayTimeArr.length > 0) {
                         weekly.Weekly_Schedule[WeekArr[i]] = []
                         for (var j = 0; j < dayTimeArr.length; j++) {
-                            var hour;
-                            var minute;
-                            var second;
-                            var hundredths;
-                            weekly.Weekly_Schedule[WeekArr[i]].push({
-                                "hour": 1,
-                                "minute": 2,
-                                "second": 3,
-                                "hundredths": 4
-                            })
+                            console.log(dayTimeArr)
+                            var starttime = new Date($(dayTimeArr[j]).attr("starttime"));
+                            var endtime = new Date($(dayTimeArr[j]).attr("endtime"));
+                            weekly.Weekly_Schedule[WeekArr[i]].push(
+                                {
+                                    time: {
+                                        "hour": starttime.getHours(),
+                                        "minute": starttime.getMinutes(),
+                                        "second": starttime.getSeconds(),
+                                        "hundredths": 0
+                                    },
+                                    value: "1"
+                                }, {
+                                    time: {
+                                        "hour": endtime.getHours(),
+                                        "minute": endtime.getMinutes(),
+                                        "second": endtime.getSeconds(),
+                                        "hundredths": 0
+                                    },
+                                    value: "0"
+                                }
+                            )
                         }
 
                     }
@@ -129,9 +144,7 @@ Ext.create('Ext.window.Window', {
         ],
 
         listeners: {
-
             el: {
-
                 contextmenu: function (win, el, eOpts) {
                     console.log(arguments)
                     //柱子间隔 27  宽100  高625
@@ -139,7 +152,6 @@ Ext.create('Ext.window.Window', {
                         return;
                     }
                     ;
-
                     Ext.create('Ext.menu.Menu', {
                         width: 100,
                         plain: true,
@@ -168,7 +180,6 @@ Ext.create('Ext.window.Window', {
                 width: 1000,
                 height: 800,
                 padding: '10 0 0 0',
-
                 store: {
                     fields: ['time', 'open', 'high', 'low', 'close'],
                     data: [{
@@ -243,12 +254,10 @@ Ext.create('Ext.window.Window', {
                         //return new Date(label).toLocaleTimeString();
                         return hours + ":" + min + ":" + sec;
                     },
-
                     title: {
                         text: 'Date',
                         fontSize: 15
                     },
-
                     style: {
                         axisLine: false
                     }
@@ -264,14 +273,14 @@ Ext.create('Ext.window.Window', {
                             }
                         },
                         /*tips: {
-                            trackMouse: true,
-                            style: 'background: #FFF',
-                            height: 20,
-                            renderer: function (storeItem, item) {
-                                this.setTitle("aa")
-                                console.log(arguments)
-                            }
-                        },*/
+                         trackMouse: true,
+                         style: 'background: #FFF',
+                         height: 20,
+                         renderer: function (storeItem, item) {
+                         this.setTitle("aa")
+                         console.log(arguments)
+                         }
+                         },*/
                         style: {
                             width: 100
                             //margin:40
@@ -291,83 +300,227 @@ Ext.create('Ext.window.Window', {
         ]
     }
 )
+function drawWindowAddDiv() {
+    var d = {
+        "Weekly_Schedule": {
+            "sunday": [{
+                "time": {"hour": 2, "minute": 8, "second": 46, "hundredths": 0},
+                "value": "1"
+            }, {"time": {"hour": 2, "minute": 55, "second": 36, "hundredths": 0}, "value": "0"}],
+            "monday": [{
+                "time": {"hour": 9, "minute": 19, "second": 36, "hundredths": 0},
+                "value": "1"
+            }, {"time": {"hour": 10, "minute": 6, "second": 26, "hundredths": 0}, "value": "0"}],
+            "thursday": [{
+                "time": {"hour": 11, "minute": 14, "second": 20, "hundredths": 0},
+                "value": "1"
+            }, {"time": {"hour": 12, "minute": 1, "second": 10, "hundredths": 0}, "value": "0"}]
+        }
+    }
+    var dw = dwPars.dw;
+    var oCanvas = dwPars.oCanvas;
+    var oneDay = dwPars.oneDay;
+    var bWidth = dwPars.bWidth;
+    var winOffsetLeft = dwPars.winOffsetLeft;
+    var winOffsetTop = dwPars.winOffsetTop;
+    var bMarginLeft = dwPars.bMarginLeft;
+    var bMaxWidth = dwPars.bMaxWidth;
+    var interval = dwPars.interval;
+    var startPoint = dwPars.startPoint;
+    var posLeftArr = dwPars.posLeftArr;
+    var oneDay = dwPars.oneDay;
+
+    for (var i = 0; i < WeekArr.length; i++) {
+        if (d['Weekly_Schedule'][WeekArr[i]]) {
+            var div = dwPars.div()
+            div.addClass(WeekArr[i])
+            var times = d['Weekly_Schedule'][WeekArr[i]];
+            for (var j = 0; j < times.length; j += 2) {
+                var starttime = new Date(1970, 1, 1, times[j].time.hour, times[j].time.minute, times[j].time.second)
+                var endtime = new Date(1970, 1, 1, times[j + 1].time.hour, times[j + 1].time.minute, times[j + 1].time.second)
+                div.attr("starttime", starttime)
+                div.attr("endtime", endtime)
+                div.addClass(WeekArr[i])
+                $(dw.el.dom).append(div)
+                weekDivAddEvent(div)
+            }
+
+
+        }
+    }
+
+
+    weekDivResetPosition()
+
+    console.log(d)
+}
+
+var single = (function () {
+    var unique;
+
+    function getInstance() {
+        if (unique === undefined) {
+            unique = new Construct();
+        }
+        return unique;
+    }
+
+    function Construct() {
+        // ... 生成单例的构造函数的代码
+    }
+
+    return {
+        getInstance: getInstance
+    }
+})();
+var dwPars;
+Ext.onReady(function () {
+    dwPars = (function () {
+
+        var WeekArrJson = [{name: "sunday", left: ""},
+            {name: "monday", left: ""},
+            {name: "tuesday", left: ""},
+            {name: "wednesday", left: ""},
+            {name: "thursday", left: ""},
+            {name: "friday", left: ""},
+            {name: "saturday", left: ""}];
+        var dw = Ext.getCmp("drawWindow")
+        var oCanvas = $(dw.el.dom.querySelectorAll("canvas")[4]);
+        var oneDay = 86400000;
+        var bWidth = 100;
+        var winOffsetLeft = dw.el.getLeft(true);
+        var winOffsetTop = dw.el.getTop(true);
+        var bMarginLeft = oCanvas.offset().left - winOffsetLeft;
+        var bMaxWidth = oCanvas.attr("width");
+        var interval = (bMaxWidth - bWidth * WeekArr.length) / 7;
+        var bMarginTop = oCanvas.offset().top - winOffsetTop - 6;
+        var startPoint = bMarginLeft + interval / 2 - 6;
+        var posLeftArr = [];
+        posLeftArr.push(startPoint)
+        WeekArrJson[0]['left'] = startPoint;
+        for (var i = 0; i < 7; i++) {
+            var weekleft = Math.ceil(startPoint += bWidth + interval);
+            posLeftArr.push(weekleft)
+            if (i < 6) {
+                WeekArrJson[i + 1]['left'] = weekleft;
+            }
+        }
+        var bMaxHeight = oCanvas.attr("height");
+        var bMarginTopHeight = parseInt(bMarginTop) + parseInt(bMaxHeight);
+
+        function newDiv() {
+            var div = $("<div></div>")
+            div.css("width", "100px")
+                .css("height", "20px")
+                .css("backgroundColor", "rgba(91,164,60,1)")
+                .css("zIndex", "9")
+                .css("position", "absolute")
+                .addClass("week")
+                .attr("id", "bar" + Math.floor(Math.random() * 100000000000000))
+            return div;
+        }
+
+        //.css("top", bMarginTop + "px");
+        //.css("top", win.pageY - winOffsetTop + "px");
+
+        var e = {
+            WeekArrJson: WeekArrJson,
+            dw: dw,
+            oCanvas: oCanvas,
+            oneDay: oneDay,
+            bWidth: bWidth,
+            winOffsetLeft: winOffsetLeft,
+            winOffsetTop: winOffsetTop,
+            bMarginLeft: bMarginLeft,
+            bMaxWidth: bMaxWidth,
+            interval: interval,
+            bMarginTop: bMarginTop,
+            startPoint: startPoint,
+            posLeftArr: posLeftArr,
+            bMaxHeight: bMaxHeight,
+            bMarginTopHeight: bMarginTopHeight,
+            div: newDiv
+        }
+        return e;
+    })()
+})
+
+
+function weekDivResetPosition() {
+    var WeekArrJson = dwPars.WeekArrJson
+    var oCanvas = dwPars.oCanvas
+    var oneDay = dwPars.oneDay;
+    var bMarginTop = dwPars.bMarginTop;
+    for (var i = 0; i < WeekArr.length; i++) {
+        var dayTimeArr = document.querySelectorAll("." + WeekArr[i]);
+        if (dayTimeArr.length > 0) {
+            for (var j = 0; j < dayTimeArr.length; j++) {
+                var starttime = new Date($(dayTimeArr[j]).attr("starttime"));
+                var divStartPageY = parseInt(oCanvas.css("height")) * ((starttime - 2649600000) / oneDay);
+                $(dayTimeArr[j]).css("top", divStartPageY + bMarginTop)
+                var endtime = new Date($(dayTimeArr[j]).attr("endtime"));
+                var divEndPageY = parseInt(oCanvas.css("height")) * ((endtime - 2649600000) / oneDay);
+                $(dayTimeArr[j]).css("height", divEndPageY - divStartPageY + "px");
+            }
+        }
+    }
+    var aWeeks = $(".week");
+    for (var i = 0; i < aWeeks.length; i++) {
+        for (var j = 0; j < WeekArrJson.length; j++) {
+            if ($(aWeeks[i]).hasClass(WeekArrJson[j].name)) {
+                $(aWeeks[i]).css("left", WeekArrJson[j].left);
+            }
+        }
+    }
+
+}
 
 function addNewBar(win) {
     console.log(win)
     //var posLeftArr;
-    var WeekArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var oCanvas=$(win.el.dom.querySelectorAll("canvas")[4]);
-    
-    var bWidth = 100;
-    var bMarginLeft = $(win.target).offset().left - $("#" + win.currentTarget.id).offset().left;
-    var bMaxWidth = win.target.width;
-    var interval = (bMaxWidth - bWidth * WeekArr.length) / 7;
-    var startPoint = bMarginLeft + interval / 2 - 6;
-    var posLeftArr = [];
-    posLeftArr.push(startPoint)
-    for (var i = 0; i < 7; i++) {
-        posLeftArr.push(Math.ceil(startPoint += bWidth + interval))
-    }
-    var bMarginTop = $(win.target).offset().top - win.currentTarget.offsetTop - 6;
-
-    var bMaxHeight = win.target.height;
-    var bMarginTopHeight = bMarginTop + bMaxHeight;
+    var WeekArr = dwPars.WeekArrJson
+    var dw = dwPars.dw;
+    //var oCanvas = dwPars.oCanvas;
+    var oneDay = dwPars.oneDay;
+    var bWidth = dwPars.bWidth;
+    var winOffsetLeft = dwPars.winOffsetLeft
+    var winOffsetTop = dwPars.winOffsetTop
+    /*var bMarginLeft =dwPars.bMarginLeft
+     var bMaxWidth =dwPars.bMaxWidth
+     var interval =dwPars.interval*/
+    var posLeftArr = dwPars.posLeftArr;
+    console.log(posLeftArr)
+    var bMarginTop = dwPars.bMarginTop
+    var bMaxHeight = dwPars.bMaxHeight
+    var bMarginTopHeight = dwPars.bMarginTopHeight
     var bLeft;
-    var winOffsetLeft = win.currentTarget.offsetLeft;
-    var oneDay = 86400000;
-    var div = $("<div></div>")
-    div.css("width", bWidth + "px")
-        .css("height", "20px")
-        .css("backgroundColor", "rgba(91,164,60,1)")
-        .css("zIndex", "9")
-        .css("top", win.pageY - win.currentTarget.offsetTop + "px")
-        .css("position", "absolute")
-        .addClass("week")
-        .attr("id", "bar" + Math.floor(Math.random() * 100000000000000));
 
+    var div = dwPars.div()
+        .css("top", win.pageY - winOffsetTop + "px");
     for (var i = 0; i < posLeftArr.length; i++) {
         if (isBarCollsion(win.pageX, win.pageY, posLeftArr[i] + winOffsetLeft, bMarginTop, bWidth, bMaxHeight)) {
             bLeft = posLeftArr[i];
-            div.addClass(WeekArr[i]);
+            div.addClass(WeekArr[i].name);
         }
     }
-
-    div.css("left", bLeft + "px")
-
-    function isBarCollsion(x1, y1, x2, y2, w, h) {
-        if (x1 >= x2 && x1 <= x2 + w && y1 >= y2 && y1 <= y2 + h) {
-            return true;
-        }
-        return false;
-    }
-
+    // div.css("left", bLeft + "px")
     if (bLeft) {
-        $(win.currentTarget).append(div)
+        $(dw.el.dom).append(div)
     } else {
         div.remove()
     }
-    div.contextmenu(function (e) {
-        e.stopPropagation()
-        Ext.create('Ext.menu.Menu', {
-            width: 100,
-            plain: true,
-            x: e.pageX,
-            y: e.pageY,
-            autoShow: true,
-            floating: true,  // usually you want this set to True (default)
-            items: [{
-                text: 'Delete Time',
-                handler: function () {
-                    div.remove()
-                }
-            }
-            ]
-        });
-        return false;
-    })
+    weekDivResetPosition()
+    weekDivAddEvent(div)
+
+
     var tmStart = getTimeByLocation(parseInt(div.css("Top")) - bMarginTop);
     var tmEnd = getTimeByLocation(parseInt(div.css("Top")) - bMarginTop + parseInt(div.css("height")))
     div.attr("startTime", tmStart).attr("endTime", tmEnd)
+
+
+}
+function weekDivAddEvent(div) {
+    console.log(div)
     div.hover(
         function () {
             //var tmStart = getTimeByLocation(parseInt(div.css("Top")) - bMarginTop);
@@ -390,41 +543,61 @@ function addNewBar(win) {
         function () {
         }
     );
-
-    function getTimeByLocation(weizhi) {
-        var time = new Date(oneDay * (weizhi / bMaxHeight) + 2649600000);
-        return time;
-    }
+    div.contextmenu(function (e) {
+        e.stopPropagation()
+        Ext.create('Ext.menu.Menu', {
+            width: 100,
+            plain: true,
+            x: e.pageX,
+            y: e.pageY,
+            autoShow: true,
+            floating: true,  // usually you want this set to True (default)
+            items: [{
+                text: 'Delete Time',
+                handler: function () {
+                    div.remove()
+                }
+            }
+            ]
+        });
+        return false;
+    })
 
     Ext.create("Ext.resizer.Resizer", {
         target: div.attr("id"),
         handles: 'n,s',
-        maxHeight: bMaxHeight,
+        maxHeight: dwPars.bMaxHeight,
         minHeight: 1,
         //constrainTo:th.id,
         pinned: true,
         listeners: {
             resize: function (th, width, height, e, eOpts) {
-                var tmStart = getTimeByLocation(parseInt(div.css("Top")) - bMarginTop);
-                var tmEnd = getTimeByLocation(parseInt(div.css("Top")) - bMarginTop + parseInt(div.css("height")))
+                var tmStart = getTimeByLocation(parseInt(div.css("Top")) - dwPars.bMarginTop);
+                var tmEnd = getTimeByLocation(parseInt(div.css("Top")) - dwPars.bMarginTop + parseInt(div.css("height")))
                 div.attr("startTime", tmStart).attr("endTime", tmEnd)
                 var top = th.el.getTop(true)
-                if (top < bMarginTop) {
-                    th.el.setTop(bMarginTop + "px")
+                if (top < dwPars.bMarginTop) {
+                    th.el.setTop(dwPars.bMarginTop + "px")
                 }
-
                 var bt = height + top;
-                if (bt > bMarginTopHeight) {
-                    var cha = bt - bMarginTopHeight;
+                if (bt > dwPars.bMarginTopHeight) {
+                    var cha = bt - dwPars.bMarginTopHeight;
                     th.el.setTop(top - cha);
-                }
 
-            },
-            resizedrag: function (th, width, height, e, eOpts) {
+                }
             }
         }
     })
 }
+function getTimeByLocation(weizhi) {
+    var time = new Date(dwPars.oneDay * (weizhi / dwPars.bMaxHeight) + 2649600000);
+    return time;
+}
 
-
+function isBarCollsion(x1, y1, x2, y2, w, h) {
+    if (x1 >= x2 && x1 <= x2 + w && y1 >= y2 && y1 <= y2 + h) {
+        return true;
+    }
+    return false;
+}
 
