679211a579d3add60c813c35a2018c78959ffa99
15075767858 <1078900559@qq.com>
2016-09-27 09:57:30 +0800

    增加图表

--- app/view/chart/RenameChart.js
+++ app/view/chart/RenameChart.js
@@ -10,10 +10,40 @@ Ext.define('svgxml.view.chart.RenameChart', {
     viewModel: {
         type: 'chart-renamechart'
     },
-
+    layout:"hbox",
     initComponent: function () {
         var me = this;
 
+        me.rbar=[
+            {text:"AI",menu:[
+                {devType:6,text:"+",handler:"addType",handler:"addType"},
+                {devType:6,text:"-",scope:chart,handler:"deleteType"}
+            ]},
+            {text:"AO",menu:[
+                {devType:5,text:"+",handler:"addType",handler:"addType"},
+                {devType:5,text:"-",scope:chart,handler:"deleteType"}
+            ]},
+            {text:"AV",menu:[
+                {devType:4,text:"+",handler:"addType",handler:"addType"},
+                {devType:4,text:"-",scope:chart,handler:"deleteType"}
+            ]},
+            {text:"BI",menu:[
+                {devType:3,text:"+",handler:"addType",handler:"addType"},
+                {devType:3,text:"-",scope:chart,handler:"deleteType"}
+            ]},
+            {text:"BO",menu:[
+                {devType:2,text:"+",handler:"addType",handler:"addType"},
+                {devType:2,text:"-",scope:chart,handler:"deleteType"}
+            ]},
+            {text:"BV",menu:[
+                {devType:1,text:"+",handler:"addType",handler:"addType"},
+                {devType:1,text:"-",scope:chart,handler:"deleteType"}
+            ]},
+            {text:"SCHDULE",menu:[
+                {devType:0,text:"+",handler:"addType",handler:"addType"},
+                {devType:0,text:"-",scope:chart,handler:"deleteType"}
+            ]}
+        ]
 
         var chart =  Ext.create({
             xtype: 'cartesian',
@@ -22,43 +52,19 @@ Ext.define('svgxml.view.chart.RenameChart', {
             flipXY:true,
             innerPadding: '0 10 0 10',
             store: {
-                fields: ['name', 'apples', 'oranges'],
-                data: [{
-                    name: 'AI',
-                    apples: 10,
-                    oranges: 3
-                }, {
-                    name: 'AO',
-                    apples: 7,
-                    oranges: 2
-                }, {
-                    name: 'AV',
-                    apples: 5,
-                    oranges: 2
-                }, {
-                    name: 'BI',
-                    apples: 2,
-                    oranges: 3
-                }, {
-                    name: 'BO',
-                    apples: 19,
-                    oranges: 1
-                }, {
-                    name: 'BV',
-                    apples: 13,
-                    oranges: 4
-                },{
-                    name:"SCHEDULE",
-                    apples:10
-                }
-                ]
+                fields: ['name', 'count', 'devs'],
+                data: me.storeData
             },
+            /*store:Ext.create("Ext.data.Store",{
+             fields: ['name', 'count', 'devs'],
+             data: me.storeData
+             }),*/
 
             axes: [{
                 type: 'numeric3d',
                 position: 'bottom',
 
-                fields: ['apples', 'oranges'],
+                fields: ['count', 'devs'],
                 title: {
                     text: 'Number',
                     fontSize: 15
@@ -85,18 +91,31 @@ Ext.define('svgxml.view.chart.RenameChart', {
                 type: 'bar3d',
                 xField: 'name',
                 position:'left',
-                yField: ['apples', 'oranges'],
+                yField: ['count'],
+                label: {
+                    field: 'count',
+                    display: 'insideEnd',
+                    //renderer: 'onSeriesLabelRender'
+                },
                 tooltip:{
                     trackMouse:true,
-                    /*renderer:function(){
+                    style:{
+                        wordWrap:"break-word"
+                    },
+                    renderer:function(record, item){
+                        console.log(this)
                         console.log(arguments)
-                    }*/
+                        this.setHtml("<div style='background-color: #0f5f99;color: white'>"+record.data.devs+"</div>")
+                    }
                 }
             }
         });
+
+
+
         me.items=chart;
 
-            me.callParent();
+        me.callParent();
     }
 });
 
--- app/view/chart/RenameChartController.js
+++ app/view/chart/RenameChartController.js
@@ -7,6 +7,8 @@ Ext.define('svgxml.view.chart.RenameChartController', {
     },
 
     onSeriesLabelRender: function (v) {
+        console.log(arguments)
+        return v
         return Ext.util.Format.number(v / 1000, '0,000');
     },
 
@@ -24,6 +26,94 @@ Ext.define('svgxml.view.chart.RenameChartController', {
         }
         var chart = this.lookupReference('chart');
         chart.preview();
+    },
+    deleteType:function(button){
+        var cartesian = this.view.down("cartesian")
+        var model = cartesian.getStore().getAt(button.devType)
+        var win = Ext.create('Ext.window.Window', {
+            title: 'Delete •••',
+            frame: true,
+            width: 325,
+            bodyPadding: 10,
+            autoShow: true,
+            defaultType: 'textfield',
+            defaults: {
+                anchor: '100%'
+            },
+            items: [
+                {
+                    margin: 10,
+                    xtype: "combobox",
+                    allowBlank: false,
+                    fieldLabel: 'select file name',
+                    store: aDevNames,
+                    editable: false,
+                    queryMode: 'local',
+                    displayField: 'name',
+                    valueField: 'name',
+                    autoSelect: false
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Ok', handler: function () {
+                    var text = win.down("combobox").getValue();
+                    if (text == null) {
+                        Ext.Msg.alert('Info', 'Plase select file name.');
+                        return;
+                    }
+                    win.close();
+                    Ext.Ajax.request({
+                        url: "resources/xmlRW.php",
+                        async: false,
+                        params: {
+                            fileName: "devsinfo/" + text,
+                            rw: "r"
+                        },
+                        success: function (response) {
+                            //var ojsonstr = response.responseText
+
+                            var tabpanel = Ext.getCmp("frametab_drawpanel");
+                            tabpanel.addDrawPanel(text)
+
+                            /*var drawpanels = Ext.ComponentQuery.query("drawpanel");
+                             for (var i = 0; i < drawpanels.length; i++) {
+                             if (drawpanels[i].title == text) {
+                             tabpanel.setActiveTab(drawpanels[i].id);
+                             return;
+                             }
+                             drawpanels[i].close()
+                             }
+
+                             var drawpanel = Ext.create("svgxml.view.tab.DrawPanel", {
+                             title: text
+                             })
+                             //console.log(tabpanel.items)
+                             tabpanel.add(drawpanel)
+                             tabpanel.setActiveTab(drawpanel.id);*/
+
+                        }
+                    })
+                    win.close();
+                }
+                },
+                {
+                    text: 'Cancel', handler: function () {
+                    win.close();
+                }
+                }
+            ]
+        })
+
+        model.set("count",model.get('count')-1)
+
+        testcartesian = cartesian;
+    },
+    addType:function(button){
+
+        var cartesian = this.view.down("cartesian")
+        var model = cartesian.getStore().getAt(button.devType)
+        model.set("count",model.get('count')+1)
     }
-    
+
 });
